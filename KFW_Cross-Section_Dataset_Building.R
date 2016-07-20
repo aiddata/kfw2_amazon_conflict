138bcd749559b13f220d8299889c975b5dba521a
John Lakwijk <jflakwijk@email.wm.edu>
2016-07-20 16:43:44 -0400

    Adds population and night lights data, merges them into main dataset

--- KFW_Cross-Section_Dataset_Building.R
+++ KFW_Cross-Section_Dataset_Building.R
@@ -14,6 +14,7 @@ rm(list = ls())
 shpfile = "KFW_Amazon/processed_data/kfw_analysis_inputs.shp"
 shpfile_working = readShapePoly(shpfile)
 
+#makes a dataframe of the shapefile data
 df_shpfile <- as.data.frame(shpfile_working)
 
 #load saved data
@@ -23,55 +24,43 @@ load("kfw2_amazon_conflict/Interim Data/Demarcation Date Data.Rda")
 load("kfw2_amazon_conflict/Interim Data/Land Violence Overlap.Rda")
 load("kfw2_amazon_conflict/Interim Data/Individual Violence Overlap.Rda")
 
+#reads the population and nighttime lights data from file
+data_pop_nlights <- read.csv(file = "kfw2_amazon_conflict/Raw and Original Data/merge_terra_indigenaPolygon_id_thin.csv", header=TRUE, sep = ',')
+
+
+#EXAMPLE for counting frequency of violence - from Preliminary_Delimitation_and_Conflict_Data_Analysis.R
+#counts frequency of each id_ad in the data (number of incidences of violence per unit)
+#iviolence_overlap$freq <- table(data_iviolence$id_ad)
+#lviolence_overlap$freq <- table(data_lviolence$id_ad)
+
 
 #renames data_iviolence$ad_id to id_ad
 names(data_iviolence)[names(data_iviolence)=="ad_id"] <- "id_ad"
 #renames lviolence_uniqueid to ad_id so the the id names match
 names(data_lviolence)[names(data_lviolence)=="ad_id"] <- "id_ad"
 
+#changes the year variables in data_iviolence and data_lviolence from factors to integers
 data_iviolence$year <- as.integer(as.character(data_iviolence$year))
 data_lviolence$year <- as.integer(as.character(data_lviolence$year))
 
 
-#counts frequency of each id_ad in the data (number of incidences of violence per unit)
-#iviolence_overlap$freq <- table(data_iviolence$id_ad)
-#lviolence_overlap$freq <- table(data_lviolence$id_ad)
+#TEST for counting violence incidents for one year - not necessary code for final product
+#iviolence_count2003 <- table(data_iviolence$id_ad[data_iviolence$year == 2003])
 
 
+#Creates new dataframes with iviolence and lviolence data and appropriately renames "freq" variable in each
 data_cross_i <- subset(iviolence_overlap, select = c("id_ad", "freq"))
 data_cross_l <- subset(lviolence_overlap, select = c("id_ad", "freq"))
-
 names(data_cross_i)[names(data_cross_i)=="freq"] <- "ifreq_total"
 names(data_cross_l)[names(data_cross_l)=="freq"] <- "lfreq_total"
 
 
-
-data_cross_i$ifreq2003 <- NA
-data_cross_i$ifreq2004 <- NA
-data_cross_i$ifreq2005 <- NA
-data_cross_i$ifreq2006 <- NA
-data_cross_i$ifreq2007 <- NA
-data_cross_i$ifreq2008 <- NA
-data_cross_i$ifreq2009 <- NA
-data_cross_i$ifreq2010 <- NA
-data_cross_i$ifreq2011 <- NA
-data_cross_i$ifreq2012 <- NA
-data_cross_i$ifreq2013 <- NA
-data_cross_i$ifreq2014 <- NA
-
-
-#data_cross_i$ifreq2003[which(data_iviolence$id_ad[year = 2003] != NULL)] <- 1
-#data_cross_i$ifreq2003[data_cross_i$id_ad == 5001] <- 1
-#data_cross_i$ifreq2003[which(data_cross_i$id_ad == data_iviolence$id_ad[data_iviolence$year == 2003 & !is.null(data_cross_i$id_ad)])] <- 1
-
-#temp_test <- as.vector(which(data_cross_i$id_ad == data_iviolence$id_ad[data_iviolence$year == 2003 & !is.null(data_cross_i$id_ad)]))
-
-#data_cross_i$ifreq2003[which(data_cross_i$id_ad == data_iviolence$id_ad[data_iviolence$year == 2003 & !is.null(data_cross_i)])] <- 1
-
-
-
+#TEST for creating new columns with violence counts - not necessary code for final product
 #data_cross_i$ifreq2003[data_cross_i$id_ad >= 5100] <- 1
 #data_cross_i$ifreq2003[is.element(data_cross_i$id_ad, data_iviolence$id_ad[data_iviolence$year == 2003])] <- 1
+
+
+#Counts the number of incidences of violence for each community in each year, and makes new columns for each year
 data_cross_i$ifreq2003[is.element(data_cross_i$id_ad, data_iviolence$id_ad[data_iviolence$year == 2003])] <- table(data_iviolence$id_ad[data_iviolence$year == 2003])
 data_cross_i$ifreq2004[is.element(data_cross_i$id_ad, data_iviolence$id_ad[data_iviolence$year == 2004])] <- table(data_iviolence$id_ad[data_iviolence$year == 2004])
 data_cross_i$ifreq2005[is.element(data_cross_i$id_ad, data_iviolence$id_ad[data_iviolence$year == 2005])] <- table(data_iviolence$id_ad[data_iviolence$year == 2005])
@@ -99,119 +88,31 @@ data_cross_l$lfreq2013[is.element(data_cross_l$id_ad, data_lviolence$id_ad[data_
 data_cross_l$lfreq2014[is.element(data_cross_l$id_ad, data_lviolence$id_ad[data_lviolence$year == 2014])] <- table(data_lviolence$id_ad[data_lviolence$year == 2014])
 
 
-
+#Creates a merged dataset with the individual and land data, as well as the demarcation dates
 data_cross_merged <- merge(data_cross_i, data_cross_l, by = "id_ad", all = TRUE)
 data_cross_merged <- merge(data_cross_merged, data_demdates, by = "id_ad", all = TRUE)
 
 
-#data_cross_i$has2003 <- length(which())
+#renames data_pop_nlights$id to data_pop_nlights$id_587
+names(data_pop_nlights)[names(data_pop_nlights)=="id"] <- "id_587"
 
-iviolence_count2003 <- table(data_iviolence$id_ad[data_iviolence$year == 2003])
+#Merges previous merged dataset with data_pop_nlights
+data_cross_merged <- merge(data_cross_merged, data_pop_nlights, by = "id_587")
 
+#Renames gpw4_*e to Pop_*e (in data_cross_merged)
+names(data_cross_merged) <- gsub("gpw4_*", "Pop_", names(data_cross_merged))
+#Removes the "e" from the end of the Pop variables
+names(data_cross_merged) <- gsub("(Pop_....)e", "\\1", names(data_cross_merged))
+#Renames ncc4_*e to ntl_*e (in data_cross_merged)
+names(data_cross_merged) <- gsub("ncc4_", "ntl_", names(data_cross_merged))
+#Removes the "e" from the end of the ntl variables
+names(data_cross_merged) <- gsub("(ntl_....)e", "\\1", names(data_cross_merged))
 
-temp_val <- sum(data_iviolence$id_ad == 5001)
-temp_vector <- sum(data_iviolence$id_ad == data_iviolence$id_ad[1:length(data_iviolence)])
 
+#Views relevant dataframes
+View(data_cross_merged)
+View(data_cross_merged[101:132])
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#iviolence_overlap$freq2003 <- length(which(data_iviolence$id_ad == iviolence_overlap$id_ad))
-
-
-
-#data_cross <- as.data.frame(data_demdates$id_ad)
-#names(data_cross)[names(data_cross)=="data_demdates$id_ad"] <- "id_ad"
-#data_cross["ifreq2003"] <- NA
-
-
-#data_cross$ifreq2003[match(iviolence_overlap$id_ad, data_demdates$id_ad)]
-#data_cross$ifreq2003[data_cross$id_ad == iviolence_overlap$id_ad] <- 5
-
-
-
-#data_cross <- as.data.frame(iviolence_overlap$id_ad)
-
-#data_cross = data_demdates
-#data_cross$id_ad <- data_iviolence$id_ad
-
-
-#data_cross$ifreq2003 <- sum(data_cross$id_ad == data_iviolence$id_ad)
-#data_cross$ifreq2003 <- table(data_iviolence$id_ad)
-
-#iviolence_overlap$ifreq2003 <- table(data_iviolence$id_ad, exclude = NULL)
-#iviolence_overlap$ifreq2003 <- sum(data_iviolence$id_ad == iviolence_overlap$id_ad)
-
-#data_cross$ifreq2004 <- table(data_iviolence$id_ad[year = 2004])
-#data_cross$ifreq2005 <- table(data_iviolence$id_ad[year = 2005])
-#data_cross$ifreq2006 <- table(data_iviolence$id_ad[year = 2006])
-#data_cross$ifreq2007 <- table(data_iviolence$id_ad[year = 2007])
-#data_cross$ifreq2008 <- table(data_iviolence$id_ad[year = 2008])
-#data_cross$ifreq2009 <- table(data_iviolence$id_ad[year = 2009])
-#data_cross$ifreq2010 <- table(data_iviolence$id_ad[year = 2010])
-#data_cross$ifreq2011 <- table(data_iviolence$id_ad[year = 2011])
-#data_cross$ifreq2012 <- table(data_iviolence$id_ad[year = 2012])
-#data_cross$ifreq2013 <- table(data_iviolence$id_ad[year = 2013])
-#data_cross$ifreq2014 <- table(data_iviolence$id_ad[year = 2014])
-
-#data_cross$lfreq2003 <- table(data_lviolence$id_ad[year = 2003])
-#data_cross$lfreq2004 <- table(data_lviolence$id_ad[year = 2004])
-#data_cross$lfreq2005 <- table(data_lviolence$id_ad[year = 2005])
-#data_cross$lfreq2006 <- table(data_lviolence$id_ad[year = 2006])
-#data_cross$lfreq2007 <- table(data_lviolence$id_ad[year = 2007])
-#data_cross$lfreq2008 <- table(data_lviolence$id_ad[year = 2008])
-#data_cross$lfreq2009 <- table(data_lviolence$id_ad[year = 2009])
-#data_cross$lfreq2010 <- table(data_lviolence$id_ad[year = 2010])
-#data_cross$lfreq2011 <- table(data_lviolence$id_ad[year = 2011])
-#data_cross$lfreq2012 <- table(data_lviolence$id_ad[year = 2012])
-#data_cross$lfreq2013 <- table(data_lviolence$id_ad[year = 2013])
-#data_cross$lfreq2014 <- table(data_lviolence$id_ad[year = 2014])
-
-
-#counts frequency of each id_ad in the data (number of incidences of violence per unit)
-#iviolence_overlap$freq <- table(data_iviolence$id_ad)
-#lviolence_overlap$freq <- table(data_lviolence$id_ad)
-
-#iviolence_overlap$freq2003 <- table(data_iviolence$id_ad)
-#lviolence_overlap$freq2003 <- iviolence_overlap$freq2003 - 
--- Preliminary_Delimitation_and_Conflict_Data_Analysis.R
+++ Preliminary_Delimitation_and_Conflict_Data_Analysis.R
@@ -1,12 +1,8 @@
 #This section is to help make the code work "out of the box" from  Github
-detach("package:MatchIt", unload=TRUE)
-load_all("~/Desktop/Github/MatchIt/R")
-# library(devtools)
-# install_github("itpir/matchit")
-library(MatchIt)
+
 
 #set the working directory to where the files are stored - !CHANGE THIS TO YOUR OWN DIRECTORY!
-setwd("/home/aiddata/Desktop/Github/kfw2_amazon_conflict")
+setwd("C:/Users/jflak/OneDrive/GitHub/kfw2_amazon_conflict")
 
 
 #Naming Guide for the following code (up until the violence data manipulation section)
@@ -1488,27 +1484,3 @@ print(paste0("FUNAI2016_reg_ever: ", FUNAI2016_reg_ever))
 #View(iviolence_overlap)
 #View(lviolence_overlap)
 
-
-#Make up a treatment
-data_iviolence["treatment.status"] <- 0
-data_iviolence["treatment.status"][data_iviolence$Case.No < 1000,] <- 1
-
-example_dta <- data_iviolence[complete.cases(data_iviolence),]
-
-nonspatial_matchit <- matchit(treatment.status ~ ad_id, data=example_dta ,
-                                 method = "nearest", distance = "logit",
-                                 caliper=0.5)
-
-#Examine our balance
-summary(nonspatial_matchit)
-
-#Subset our data to only include matches
-matchedData <- match.data(nonspatial_matchit)
-
-
-spatial.opts <- list(decay.model = "threshold",
-                     threshold = 1000)
-
-spatial_matchit <- matchit(treatment.status ~ ad_id, data= example_dta,
-                              method = "nearest", distance = "logit",
-                              caliper=0.5, spatial.options=spatial.opts)
