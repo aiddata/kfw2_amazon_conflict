add0598bdfd2adaa42d493fa2cb8e07ec66dd792
saecan <jflakwijk@email.wm.edu>
2016-07-20 14:09:08 -0400

    Adding changes by Dan

---
+++ Dan_matchit_example.R
@@ -0,0 +1,44 @@
+#This file includes the code from the example that Dan created for MatchIt
+
+library(MatchIt)
+library(devtools)
+detach("package:MatchIt", unload=TRUE)
+#load_all("~/Desktop/Github/MatchIt/R")
+# library(devtools)
+# install_github("itpir/matchit")
+
+
+#set the working directory to where the files are stored - !CHANGE THIS TO YOUR OWN DIRECTORY!
+setwd("/home/aiddata/Desktop/Github/kfw2_amazon_conflict")
+
+
+#load saved data
+load("Interim Data/Land Violence Data.Rda")
+load("Interim Data/Individual Violence Data.Rda")
+load("Interim Data/Demarcation Date Data.Rda")
+load("Interim Data/Land Violence Overlap.Rda")
+load("Interim Data/Individual Violence Overlap.Rda")
+
+#Make up a treatment
+data_iviolence["treatment.status"] <- 0
+data_iviolence["treatment.status"][data_iviolence$Case.No < 1000,] <- 1
+
+example_dta <- data_iviolence[complete.cases(data_iviolence),]
+
+nonspatial_matchit <- matchit(treatment.status ~ ad_id, data=example_dta ,
+                              method = "nearest", distance = "logit",
+                              caliper=0.5)
+
+#Examine our balance
+summary(nonspatial_matchit)
+
+#Subset our data to only include matches
+matchedData <- match.data(nonspatial_matchit)
+
+
+spatial.opts <- list(decay.model = "threshold",
+                     threshold = 1000)
+
+spatial_matchit <- matchit(treatment.status ~ ad_id, data= example_dta,
+                           method = "nearest", distance = "logit",
+                           caliper=0.5, spatial.options=spatial.opts)
