e174c47c6c197e39c59a827a29ca183a645385fe
rtrichler <rbtrichler@ip-128-239-186-30.v4.wm.edu>
2016-09-13 16:19:32 -0400

    fix NAs showing up in panel data
    trim years to 2003-2014; change reshape code

--- KFW_Panel_Dataset_Building.R
+++ KFW_Panel_Dataset_Building.R
@@ -47,18 +47,18 @@ dta_shp@data$ntl_2013[dta_shp@data$ntl_2013 == 5555] <- NA
 
 
 #Fills in the previous Pop value for the missing Pop values
-dta_shp@data$Pop_1991 <- dta_shp@data$Pop_1990
-dta_shp@data$Pop_1992 <- dta_shp@data$Pop_1990
-dta_shp@data$Pop_1993 <- dta_shp@data$Pop_1990
-dta_shp@data$Pop_1994 <- dta_shp@data$Pop_1990
+# dta_shp@data$Pop_1991 <- dta_shp@data$Pop_1990
+# dta_shp@data$Pop_1992 <- dta_shp@data$Pop_1990
+# dta_shp@data$Pop_1993 <- dta_shp@data$Pop_1990
+# dta_shp@data$Pop_1994 <- dta_shp@data$Pop_1990
+# 
+# dta_shp@data$Pop_1996 <- dta_shp@data$Pop_1995
+# dta_shp@data$Pop_1997 <- dta_shp@data$Pop_1995
+# dta_shp@data$Pop_1998 <- dta_shp@data$Pop_1995
+# dta_shp@data$Pop_1999 <- dta_shp@data$Pop_1995
 
-dta_shp@data$Pop_1996 <- dta_shp@data$Pop_1995
-dta_shp@data$Pop_1997 <- dta_shp@data$Pop_1995
-dta_shp@data$Pop_1998 <- dta_shp@data$Pop_1995
-dta_shp@data$Pop_1999 <- dta_shp@data$Pop_1995
-
-dta_shp@data$Pop_2001 <- dta_shp@data$Pop_2000
-dta_shp@data$Pop_2002 <- dta_shp@data$Pop_2000
+#dta_shp@data$Pop_2001 <- dta_shp@data$Pop_2000
+#dta_shp@data$Pop_2002 <- dta_shp@data$Pop_2000
 dta_shp@data$Pop_2003 <- dta_shp@data$Pop_2000
 dta_shp@data$Pop_2004 <- dta_shp@data$Pop_2000
 
@@ -72,190 +72,201 @@ dta_shp@data$Pop_2012 <- dta_shp@data$Pop_2010
 dta_shp@data$Pop_2013 <- dta_shp@data$Pop_2010
 dta_shp@data$Pop_2014 <- dta_shp@data$Pop_2010
 
-dta_shp@data$Pop_2016 <- dta_shp@data$Pop_2015
-dta_shp@data$Pop_2017 <- dta_shp@data$Pop_2015
-dta_shp@data$Pop_2018 <- dta_shp@data$Pop_2015
-dta_shp@data$Pop_2019 <- dta_shp@data$Pop_2015
+# dta_shp@data$Pop_2016 <- dta_shp@data$Pop_2015
+# dta_shp@data$Pop_2017 <- dta_shp@data$Pop_2015
+# dta_shp@data$Pop_2018 <- dta_shp@data$Pop_2015
+# dta_shp@data$Pop_2019 <- dta_shp@data$Pop_2015
+
+#Turn dta_shp@data into dataframe
+df<-dta_shp@data
+
+#Drop all variables for years outside of 2003-2014
+df<- df[, -grep("(19[0-9][0-9])", names(df))]
+df<- df[, -grep("(2000)", names(df))]
+df<- df[, -grep("(2001)", names(df))]
+df<- df[, -grep("(2002)", names(df))]
+df<- df[, -grep("(2015)", names(df))]
+df<- df[, -grep("(2020)", names(df))]
+
+#Drop all AvgD and AvgDist varialbes (many NAs, not used for analysis)
+df<-df[,-grep("(AvgD)",names(df))]
+
+#Add underscore to ifreq and lfreq variables to match other variables with yearly measures
+
+for (i in 2:length(df))
+{
+  colnames(df)[i] <- sub("lfreq","lfreq_",colnames(df)[i])
+}
+
+for (i in 2:length(df))
+{
+  colnames(df)[i] <- sub("ifreq","ifreq_",colnames(df)[i])
+}
 
 
-#Fills in some variables to fix an error that reshape throws
-dta_shp@data$ifreq1982 <- NA
-dta_shp@data$ifreq1983 <- NA
-dta_shp@data$ifreq1984 <- NA
-dta_shp@data$ifreq1985 <- NA
-dta_shp@data$ifreq1986 <- NA
-dta_shp@data$ifreq1987 <- NA
-dta_shp@data$ifreq1988 <- NA
-dta_shp@data$ifreq1989 <- NA
-dta_shp@data$ifreq1990 <- NA
-dta_shp@data$ifreq1991 <- NA
-dta_shp@data$ifreq1992 <- NA
-dta_shp@data$ifreq1993 <- NA
-dta_shp@data$ifreq1994 <- NA
-dta_shp@data$ifreq1995 <- NA
-dta_shp@data$ifreq1996 <- NA
-dta_shp@data$ifreq1997 <- NA
-dta_shp@data$ifreq1998 <- NA
-dta_shp@data$ifreq1999 <- NA
-dta_shp@data$ifreq2000 <- NA
-dta_shp@data$ifreq2001 <- NA
-dta_shp@data$ifreq2002 <- NA
-
-dta_shp@data$ifreq2015 <- NA
-dta_shp@data$ifreq2016 <- NA
-dta_shp@data$ifreq2017 <- NA
-dta_shp@data$ifreq2018 <- NA
-dta_shp@data$ifreq2019 <- NA
-dta_shp@data$ifreq2020 <- NA
-
-dta_shp@data$lfreq1982 <- NA
-dta_shp@data$lfreq1983 <- NA
-dta_shp@data$lfreq1984 <- NA
-dta_shp@data$lfreq1985 <- NA
-dta_shp@data$lfreq1986 <- NA
-dta_shp@data$lfreq1987 <- NA
-dta_shp@data$lfreq1988 <- NA
-dta_shp@data$lfreq1989 <- NA
-dta_shp@data$lfreq1990 <- NA
-dta_shp@data$lfreq1991 <- NA
-dta_shp@data$lfreq1992 <- NA
-dta_shp@data$lfreq1993 <- NA
-dta_shp@data$lfreq1994 <- NA
-dta_shp@data$lfreq1995 <- NA
-dta_shp@data$lfreq1996 <- NA
-dta_shp@data$lfreq1997 <- NA
-dta_shp@data$lfreq1998 <- NA
-dta_shp@data$lfreq1999 <- NA
-dta_shp@data$lfreq2000 <- NA
-dta_shp@data$lfreq2001 <- NA
-dta_shp@data$lfreq2002 <- NA
-
-dta_shp@data$lfreq2015 <- NA
-dta_shp@data$lfreq2016 <- NA
-dta_shp@data$lfreq2017 <- NA
-dta_shp@data$lfreq2018 <- NA
-dta_shp@data$lfreq2019 <- NA
-dta_shp@data$lfreq2020 <- NA
-
-dta_shp@data$MaxL_2015 <- NA
-dta_shp@data$MaxL_2016 <- NA
-dta_shp@data$MaxL_2017 <- NA
-dta_shp@data$MaxL_2018 <- NA
-dta_shp@data$MaxL_2019 <- NA
-dta_shp@data$MaxL_2020 <- NA
-
-dta_shp@data$MaxP_2015 <- NA
-dta_shp@data$MaxP_2016 <- NA
-dta_shp@data$MaxP_2017 <- NA
-dta_shp@data$MaxP_2018 <- NA
-dta_shp@data$MaxP_2019 <- NA
-dta_shp@data$MaxP_2020 <- NA
-
-dta_shp@data$MaxT_2015 <- NA
-dta_shp@data$MaxT_2016 <- NA
-dta_shp@data$MaxT_2017 <- NA
-dta_shp@data$MaxT_2018 <- NA
-dta_shp@data$MaxT_2019 <- NA
-dta_shp@data$MaxT_2020 <- NA
-
-dta_shp@data$MeanL_2015 <- NA
-dta_shp@data$MeanL_2016 <- NA
-dta_shp@data$MeanL_2017 <- NA
-dta_shp@data$MeanL_2018 <- NA
-dta_shp@data$MeanL_2019 <- NA
-dta_shp@data$MeanL_2020 <- NA
-
-dta_shp@data$MeanP_2015 <- NA
-dta_shp@data$MeanP_2016 <- NA
-dta_shp@data$MeanP_2017 <- NA
-dta_shp@data$MeanP_2018 <- NA
-dta_shp@data$MeanP_2019 <- NA
-dta_shp@data$MeanP_2020 <- NA
-
-dta_shp@data$MeanT_2015 <- NA
-dta_shp@data$MeanT_2016 <- NA
-dta_shp@data$MeanT_2017 <- NA
-dta_shp@data$MeanT_2018 <- NA
-dta_shp@data$MeanT_2019 <- NA
-dta_shp@data$MeanT_2020 <- NA
-
-dta_shp@data$MinP_2015 <- NA
-dta_shp@data$MinP_2016 <- NA
-dta_shp@data$MinP_2017 <- NA
-dta_shp@data$MinP_2018 <- NA
-dta_shp@data$MinP_2019 <- NA
-dta_shp@data$MinP_2020 <- NA
-
-dta_shp@data$MinT_2015 <- NA
-dta_shp@data$MinT_2016 <- NA
-dta_shp@data$MinT_2017 <- NA
-dta_shp@data$MinT_2018 <- NA
-dta_shp@data$MinT_2019 <- NA
-dta_shp@data$MinT_2020 <- NA
-
-dta_shp@data$ntl_1982 <- NA
-dta_shp@data$ntl_1983 <- NA
-dta_shp@data$ntl_1984 <- NA
-dta_shp@data$ntl_1985 <- NA
-dta_shp@data$ntl_1986 <- NA
-dta_shp@data$ntl_1987 <- NA
-dta_shp@data$ntl_1988 <- NA
-dta_shp@data$ntl_1989 <- NA
-dta_shp@data$ntl_1990 <- NA
-dta_shp@data$ntl_1991 <- NA
-
-#dta_shp@data$ntl_2014 <- NA
-dta_shp@data$ntl_2015 <- NA
-dta_shp@data$ntl_2016 <- NA
-dta_shp@data$ntl_2017 <- NA
-dta_shp@data$ntl_2018 <- NA
-dta_shp@data$ntl_2019 <- NA
-dta_shp@data$ntl_2020 <- NA
-
-dta_shp@data$Pop_1982 <- NA
-dta_shp@data$Pop_1983 <- NA
-dta_shp@data$Pop_1984 <- NA
-dta_shp@data$Pop_1985 <- NA
-dta_shp@data$Pop_1986 <- NA
-dta_shp@data$Pop_1987 <- NA
-dta_shp@data$Pop_1988 <- NA
-dta_shp@data$Pop_1989 <- NA
+# #Fills in some variables to fix an error that reshape throws
+# dta_shp@data$ifreq1982 <- NA
+# dta_shp@data$ifreq1983 <- NA
+# dta_shp@data$ifreq1984 <- NA
+# dta_shp@data$ifreq1985 <- NA
+# dta_shp@data$ifreq1986 <- NA
+# dta_shp@data$ifreq1987 <- NA
+# dta_shp@data$ifreq1988 <- NA
+# dta_shp@data$ifreq1989 <- NA
+# dta_shp@data$ifreq1990 <- NA
+# dta_shp@data$ifreq1991 <- NA
+# dta_shp@data$ifreq1992 <- NA
+# dta_shp@data$ifreq1993 <- NA
+# dta_shp@data$ifreq1994 <- NA
+# dta_shp@data$ifreq1995 <- NA
+# dta_shp@data$ifreq1996 <- NA
+# dta_shp@data$ifreq1997 <- NA
+# dta_shp@data$ifreq1998 <- NA
+# dta_shp@data$ifreq1999 <- NA
+# dta_shp@data$ifreq2000 <- NA
+# dta_shp@data$ifreq2001 <- NA
+# dta_shp@data$ifreq2002 <- NA
+# 
+# dta_shp@data$ifreq2015 <- NA
+# dta_shp@data$ifreq2016 <- NA
+# dta_shp@data$ifreq2017 <- NA
+# dta_shp@data$ifreq2018 <- NA
+# dta_shp@data$ifreq2019 <- NA
+# dta_shp@data$ifreq2020 <- NA
+# 
+# dta_shp@data$lfreq1982 <- NA
+# dta_shp@data$lfreq1983 <- NA
+# dta_shp@data$lfreq1984 <- NA
+# dta_shp@data$lfreq1985 <- NA
+# dta_shp@data$lfreq1986 <- NA
+# dta_shp@data$lfreq1987 <- NA
+# dta_shp@data$lfreq1988 <- NA
+# dta_shp@data$lfreq1989 <- NA
+# dta_shp@data$lfreq1990 <- NA
+# dta_shp@data$lfreq1991 <- NA
+# dta_shp@data$lfreq1992 <- NA
+# dta_shp@data$lfreq1993 <- NA
+# dta_shp@data$lfreq1994 <- NA
+# dta_shp@data$lfreq1995 <- NA
+# dta_shp@data$lfreq1996 <- NA
+# dta_shp@data$lfreq1997 <- NA
+# dta_shp@data$lfreq1998 <- NA
+# dta_shp@data$lfreq1999 <- NA
+# dta_shp@data$lfreq2000 <- NA
+# dta_shp@data$lfreq2001 <- NA
+# dta_shp@data$lfreq2002 <- NA
+# 
+# dta_shp@data$lfreq2015 <- NA
+# dta_shp@data$lfreq2016 <- NA
+# dta_shp@data$lfreq2017 <- NA
+# dta_shp@data$lfreq2018 <- NA
+# dta_shp@data$lfreq2019 <- NA
+# dta_shp@data$lfreq2020 <- NA
+# 
+# dta_shp@data$MaxL_2015 <- NA
+# dta_shp@data$MaxL_2016 <- NA
+# dta_shp@data$MaxL_2017 <- NA
+# dta_shp@data$MaxL_2018 <- NA
+# dta_shp@data$MaxL_2019 <- NA
+# dta_shp@data$MaxL_2020 <- NA
+# 
+# dta_shp@data$MaxP_2015 <- NA
+# dta_shp@data$MaxP_2016 <- NA
+# dta_shp@data$MaxP_2017 <- NA
+# dta_shp@data$MaxP_2018 <- NA
+# dta_shp@data$MaxP_2019 <- NA
+# dta_shp@data$MaxP_2020 <- NA
+# 
+# dta_shp@data$MaxT_2015 <- NA
+# dta_shp@data$MaxT_2016 <- NA
+# dta_shp@data$MaxT_2017 <- NA
+# dta_shp@data$MaxT_2018 <- NA
+# dta_shp@data$MaxT_2019 <- NA
+# dta_shp@data$MaxT_2020 <- NA
+# 
+# dta_shp@data$MeanL_2015 <- NA
+# dta_shp@data$MeanL_2016 <- NA
+# dta_shp@data$MeanL_2017 <- NA
+# dta_shp@data$MeanL_2018 <- NA
+# dta_shp@data$MeanL_2019 <- NA
+# dta_shp@data$MeanL_2020 <- NA
+# 
+# dta_shp@data$MeanP_2015 <- NA
+# dta_shp@data$MeanP_2016 <- NA
+# dta_shp@data$MeanP_2017 <- NA
+# dta_shp@data$MeanP_2018 <- NA
+# dta_shp@data$MeanP_2019 <- NA
+# dta_shp@data$MeanP_2020 <- NA
+# 
+# dta_shp@data$MeanT_2015 <- NA
+# dta_shp@data$MeanT_2016 <- NA
+# dta_shp@data$MeanT_2017 <- NA
+# dta_shp@data$MeanT_2018 <- NA
+# dta_shp@data$MeanT_2019 <- NA
+# dta_shp@data$MeanT_2020 <- NA
+# 
+# dta_shp@data$MinP_2015 <- NA
+# dta_shp@data$MinP_2016 <- NA
+# dta_shp@data$MinP_2017 <- NA
+# dta_shp@data$MinP_2018 <- NA
+# dta_shp@data$MinP_2019 <- NA
+# dta_shp@data$MinP_2020 <- NA
+# 
+# dta_shp@data$MinT_2015 <- NA
+# dta_shp@data$MinT_2016 <- NA
+# dta_shp@data$MinT_2017 <- NA
+# dta_shp@data$MinT_2018 <- NA
+# dta_shp@data$MinT_2019 <- NA
+# dta_shp@data$MinT_2020 <- NA
+# 
+# dta_shp@data$ntl_1982 <- NA
+# dta_shp@data$ntl_1983 <- NA
+# dta_shp@data$ntl_1984 <- NA
+# dta_shp@data$ntl_1985 <- NA
+# dta_shp@data$ntl_1986 <- NA
+# dta_shp@data$ntl_1987 <- NA
+# dta_shp@data$ntl_1988 <- NA
+# dta_shp@data$ntl_1989 <- NA
+# dta_shp@data$ntl_1990 <- NA
+# dta_shp@data$ntl_1991 <- NA
+# 
+# #dta_shp@data$ntl_2014 <- NA
+# dta_shp@data$ntl_2015 <- NA
+# dta_shp@data$ntl_2016 <- NA
+# dta_shp@data$ntl_2017 <- NA
+# dta_shp@data$ntl_2018 <- NA
+# dta_shp@data$ntl_2019 <- NA
+# dta_shp@data$ntl_2020 <- NA
+# 
+# dta_shp@data$Pop_1982 <- NA
+# dta_shp@data$Pop_1983 <- NA
+# dta_shp@data$Pop_1984 <- NA
+# dta_shp@data$Pop_1985 <- NA
+# dta_shp@data$Pop_1986 <- NA
+# dta_shp@data$Pop_1987 <- NA
+# dta_shp@data$Pop_1988 <- NA
+# dta_shp@data$Pop_1989 <- NA
 
 
 #Orders the dataframe columns alphabetically
 #dta_shp@data <- as.data.frame(dta_shp@data)[order(names(dta_shp@data))]
 
 #List the names of the variables in dta_shp
-#print(names(dta_shp@data))
+#print(names(df))
 
 #shows what variables are taken by reshape in varying
-#print(grep("(.+)[0-9][0-9][0-9][0-9]$", names(dta_shp), value = TRUE))
+print(grep("(.+)[0-9][0-9][0-9][0-9]$", names(df), value = TRUE))
 
 #Reshapes the dataframe from wide to long
-panel_data <- reshape(dta_shp@data, dir = "long", varying = grep("(.+)[0-9][0-9][0-9][0-9]$", names(dta_shp), value = TRUE),
-                      v.names = c("Pop_", "MeanL_", "MaxL_", "MeanT_", "MaxT_", "MinT_", "MeanP_", "MaxP_", "MinP_",
-                                  "ifreq", "lfreq", "ntl_"), times = 1982:2020)
-#scratch
-panel_data1 <- reshape(data, dir = "long", varying = grep("(.+)[0-9][0-9][0-9][0-9]$", names(data), value = TRUE),
-                      v.names = c("Pop_", "MeanL_", "MaxL_", "MeanT_", "MaxT_", "MinT_", "MeanP_", "MaxP_", "MinP_",
-                                  "ifreq", "lfreq", "ntl_"), idvar="id",timevar="year")
 
-data<-dta_shp@data
+# panel_data <- reshape(dta_shp@data, dir = "long", varying = grep("(.+)[0-9][0-9][0-9][0-9]$", names(dta_shp), value = TRUE),
+#                       v.names = c("Pop_", "MeanL_", "MaxL_", "MeanT_", "MaxT_", "MinT_", "MeanP_", "MaxP_", "MinP_",
+#                                   "ifreq", "lfreq", "ntl_"), times = 1982:2020)
 
-all_reshape <- c(Pop,MeanL, MaxL, MeanT,MaxT,MinT,MeanP,MaxP,MinP,ifreq,lfreq,ntl)
-DFa4 <- reshape(data, varying=c("Pop", "MeanL", "MaxL", "MeanT", "MaxT", "MinT", "MeanP", "MaxP", "MinP",
-                              "ntl"),direction="long", idvar="id", sep="_", timevar="year")
-
-
-all_reshape <- c(PCloss, mean_ln, minairTemp, maxairTemp, meanairTemp, minPre, maxPre, meanPre, MinDist, DecayDist, ProjCount, DecayDist100)
-DFa4 <- reshape(DFa3, varying=all_reshape,direction="long", idvar="ID", sep="_", timevar="Year")
-#------
+panel_data<-reshape(df,dir="long",varying=grep("(.+)[0-9][0-9][0-9][0-9]$", names(df), value = TRUE),sep="_")
 
 View(as.data.frame(panel_data)[1:100])
-View(as.data.frame(panel_data)[101:166])
-
-panel_data2<-panel_data[panel_data$time>=2003,]
-panel_data2<-panel_data2[panel_data2$time<=2014,]
+View(as.data.frame(panel_data)[101:147])
 
 #Rename variable "time" to "year"
 names(panel_data)[names(panel_data)=="time"]="year"
