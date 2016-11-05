21fbfd27fe8ec0ec7938e1cf02c2158ed4ed13e4
rtrichler <rbtrichler@Rachels-MacBook-Pro.local>
2016-11-05 09:33:07 -0400

    CMREG

--- Deforestation_Outcome/Panel_PPTAL_dem.R
+++ Deforestation_Outcome/Panel_PPTAL_dem.R
@@ -10,8 +10,8 @@ rm(list=ls())
 
 #set the working directory to where the files are stored - !CHANGE THIS TO YOUR OWN DIRECTORY!
 #setwd("/home/aiddata/Desktop/Github/kfw2_amazon_conflict/")
-setwd("C:/Users/jflak/OneDrive/GitHub/kfw2_amazon_conflict/")
-#setwd("/Users/rbtrichler/Documents/AidData/Git Repos/kfw2_amazon_conflict")
+#setwd("C:/Users/jflak/OneDrive/GitHub/kfw2_amazon_conflict/")
+setwd("/Users/rbtrichler/Documents/AidData/Git Repos/kfw2_amazon_conflict")
 
 #essential spatial view packages (load and project shapefiles etc...)
 library(rgdal)
@@ -45,7 +45,7 @@ panel_data<-panel_data_sub
 
 #Create treatment var that measures years prior to or after demarcation
 panel_data$trt_dem<-NA
-panel_data$trt_dem<-panel_data$year-panel_data$appr_y16
+panel_data$trt_dem<-panel_data$year-panel_data$demend_y
 
 #test treatment binary
 panel_data_sort<-panel_data[order(panel_data$id),]
@@ -58,7 +58,7 @@ View(as.data.frame(panel_data_sort)[,100:149])
 Model1<- lm(MaxL ~ trt_dem + factor(id), data=panel_data)
 cluster1 <- cluster.vcov(Model1, cbind(panel_data$year, panel_data$id), force_posdef=TRUE)
 CMREG1 <- coeftest(Model1, cluster1)
-print(summary(Model1))
+CMREG1
 
 Model2<- lm(MaxL ~ trt_dem + 
               lfreq + Pop + 
@@ -69,7 +69,7 @@ Model2<- lm(MaxL ~ trt_dem +
             data=panel_data)
 cluster2 <- cluster.vcov(Model2, cbind(panel_data$year, panel_data$id), force_posdef=TRUE)
 CMREG2 <- coeftest(Model2, cluster2)
-print(summary(Model2))
+CMREG2
 
 Model3<- lm(MaxL ~ trt_dem + 
               lfreq + Pop + 
@@ -80,15 +80,18 @@ Model3<- lm(MaxL ~ trt_dem +
             data=panel_data)
 cluster3 <- cluster.vcov(Model3, cbind(panel_data$year, panel_data$id), force_posdef=TRUE)
 CMREG3 <- coeftest(Model3, cluster3)
-print(summary(Model3))
+CMREG3
 
-Model4<- lm(MaxL ~ trt_dem + 
-              Pop + ntl +
-              year + factor(id),
+Model4<- lm(MaxL ~ trt_dem +
+              lfreq + Pop +
+              MeanT + MinT + MaxT +
+              MeanP + MinP + MaxP +
+              ifreq + ntl +
+              factor(year) + factor(id),
             data=panel_data)
 cluster4 <- cluster.vcov(Model4, cbind(panel_data$year, panel_data$id), force_posdef=TRUE)
 CMREG4 <- coeftest(Model4, cluster4)
-print(summary(Model4))
+CMREG4
 
 
 #-------------------
@@ -99,11 +102,11 @@ stargazer(CMREG1,CMREG2,CMREG3,CMREG4,
           type="html", align=TRUE,
           omit=c("factor"), omit.label=c("factor"),
           omit.stat=c("f","ser"),
-          add.lines=list(c("Observations","3708","3708","3708","3708"),
+          add.lines=list(c("Observations","1272","1272","1272","1272"),
                          c("Community Fixed Effects?","Yes","Yes","Yes","Yes"),
                          c("Year Fixed Effects?","No","No","No","Yes")),
-          title="Full Sample Regression Results: Ever Demarcated",
-          dep.var.labels=c("Land Conflict"))
+          title="PPTAL Regression Results: Ever Demarcated",
+          dep.var.labels=c("Land Cover"))
 
 
 
